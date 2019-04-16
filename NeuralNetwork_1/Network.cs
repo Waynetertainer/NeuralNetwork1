bb020eb2d545b55ae9dc621a24c98b8e10aa32d6
Unknown <joshua.tobias@games-academy.de>
2019-04-16 18:06:11 +0200

    init

---
+++ NeuralNetwork_1/Network.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork_1
+{
+    public class Network
+    {
+        private int mLayerCount;
+        private int[] mLayerSizes;
+
+        public Network(int[] input)
+        {
+            mLayerCount = input.Length;
+            mLayerSizes = input;
+
+        }
+    }
+}
